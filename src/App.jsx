4bc30a974bb40fa6da3c898905e041d04a3c7b16
Timothy Adesina <timobest121@gmail.com>
2024-12-16 11:30:18 +0100

    indebt

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { useState } from "react";
 function App() {
 
   const [step, setStep] = useState (1);
+  const [isOpen, setIsOpen] = useState (true);
    
   function handlePrevious () {
    if (step > 1) setStep(step - 1)
@@ -22,8 +23,13 @@ function App() {
      if (step < 3) setStep(step + 1)
   }
 
-  return (
-   <div className="steps">
+    return (
+    <div>
+
+      <button className="close" onClick={ () => setIsOpen(!isOpen)}>&times;</button>
+
+    { isOpen && 
+      <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
       <div className={`${step >= 2 ? "active" : ""}`}>2</div>
@@ -39,8 +45,10 @@ function App() {
       <button style = {{backgroundColor: "#7950f2", color: "#fff"}} onClick={() =>
         handleNext()}>Next</button>
     </div>
-   </div>
-  )
+    </div> }
+
+    </div>
+    )
 }
 
 export default App
